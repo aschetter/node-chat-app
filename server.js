4a9894a218ab753954845f4460c4780d5b9ef3c8
Andy Schetter <andyschetter@gmail.com>
2014-10-01 13:57:13 -0500

    Add check to ensure a message has a name and content

--- server.js
+++ server.js
@@ -10,13 +10,13 @@ function handler (req, res) {
     fs.readFile(__dirname + '/index.html',
 
     function (err, data) {
-    if (err) {
-        res.writeHead(500);
-        return res.end('Error loading index.html');
-    }
+        if (err) {
+            res.writeHead(500);
+            return res.end('Error loading index.html');
+        }
 
-    res.writeHead(200);
-    res.end(data);
+        res.writeHead(200);
+        res.end(data);
     });
 }
 
@@ -33,14 +33,21 @@ MongoClient.connect('mongodb://127.0.0.1/chat', function(err, db) {
         socket.on('confirmConnection', function (data) {
             console.log(data);
         });
+
         socket.on('toServer', function (data) {
             console.log(data);
+
             var name = data.name;
             var message = data.message;
+            var whiteSpaceCheck = /^\s*$/;
 
-            collection.insert({name: name, message: message}, function() {
-                console.log('inserted');
-            });
+            if (whiteSpaceCheck.test(name) || whiteSpaceCheck.test(message)) {
+                console.log('Invalid');
+            } else {
+                collection.insert({name: name, message: message}, function() {
+                    console.log('Inserted');
+                });
+            }
         });
     });
 });
