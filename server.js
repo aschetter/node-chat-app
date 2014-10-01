722b5d77b38067608cf472c970f62d1192fe9547
Andy Schetter <andyschetter@gmail.com>
2014-10-01 13:06:53 -0500

    Connect node server to mongodb

--- server.js
+++ server.js
@@ -1,27 +1,46 @@
-// var mongo = require('mongodb').MongoClient;
-// var client = require('socket.io').listen(8080).sockets;
+var MongoClient = require('mongodb').MongoClient;
 
-var app = require('http').createServer(handler)
+var app = require('http').createServer(handler);
 var io = require('socket.io')(app);
+var fs = require('fs');
 
 app.listen(8080);
 
 function handler (req, res) {
-  fs.readFile(__dirname + '/index.html',
-  function (err, data) {
+    fs.readFile(__dirname + '/index.html',
+
+    function (err, data) {
     if (err) {
-      res.writeHead(500);
-      return res.end('Error loading index.html');
+        res.writeHead(500);
+        return res.end('Error loading index.html');
     }
 
     res.writeHead(200);
     res.end(data);
-  });
+    });
 }
 
-io.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
-  socket.on('my other event', function (data) {
-    console.log(data);
-  });
+MongoClient.connect('mongodb://127.0.0.1/chat', function(err, db) {
+    if (err) {
+        throw err;
+    }
+
+    io.on('connection', function (socket) {
+
+        var collection = db.collection('messages');
+
+        socket.emit('connected', 'You are connected to the chat server!');
+        socket.on('confirmConnection', function (data) {
+            console.log(data);
+        });
+        socket.on('toServer', function (data) {
+            console.log(data);
+            var name = data.name;
+            var message = data.message;
+
+            collection.insert({name: name, message: message}, function() {
+                console.log('inserted');
+            });
+        });
+    });
 });
