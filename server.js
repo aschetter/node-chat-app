c8d54580e05877d9b8c505d5996614c4794ad3d5
Andy Schetter <andyschetter@gmail.com>
2014-09-30 20:49:00 -0500

    Add http server and complete first test sending client message to server

--- server.js
+++ server.js
@@ -1,1 +1,27 @@
-console.log('Hello')
+// var mongo = require('mongodb').MongoClient;
+// var client = require('socket.io').listen(8080).sockets;
+
+var app = require('http').createServer(handler)
+var io = require('socket.io')(app);
+
+app.listen(8080);
+
+function handler (req, res) {
+  fs.readFile(__dirname + '/index.html',
+  function (err, data) {
+    if (err) {
+      res.writeHead(500);
+      return res.end('Error loading index.html');
+    }
+
+    res.writeHead(200);
+    res.end(data);
+  });
+}
+
+io.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+});
